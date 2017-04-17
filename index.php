ff272fd80f63682c35da55642e0935fd180d11f2
Andrew Kiselik <kisela@rpi.edu>
2017-04-17 17:49:44 -0400

    Added photo to index.

--- index.php
+++ index.php
@@ -20,31 +20,33 @@
 <body>
   <div class="well">
     <div class="text-center">
-        <h1>Welcome to the RPI O2CM Fix</h1>
-        <h4><small>Please select if you'd like to add a new dancer or register for a new competion<small><h4>
-        <button type="button" id="comp" class="btn btn-primary">Register a Couple for a Compeititon</button>
-        <button type="button" id="newcomp" class="btn btn-info">Register for a New Competition</button>
-        <button type="button" id="dancer" class="btn btn-default">New Dancer</button>
-        <button type="button" id="univ" class="btn btn-success">New University</button>
-        <button type="button" id="users" class="btn btn-basic">View All Dancers</button>
+      <h1>Welcome to the RPI O2CM Fix</h1>
+      <div class="center">
+        <img class="img-rounded" src="https://scontent.xx.fbcdn.net/v/t1.0-9/17799453_1904458669767073_5514341174671551245_n.jpg?oh=672148d26a9452100ecf4c599ecbc27f&oe=5991B578" alt="RPI Team Photo">
+        <div>
+          <button type="button" id="comp" class="btn btn-primary">Register a Couple for a Compeititon</button>
+          <button type="button" id="newcomp" class="btn btn-info">Register for a New Competition</button>
+          <button type="button" id="dancer" class="btn btn-default">New Dancer</button>
+          <button type="button" id="univ" class="btn btn-success">New University</button>
+          <button type="button" id="users" class="btn btn-basic">View All Dancers</button>
+        </div>
       </div>
-  </div>
-  <script>
-  $('#comp').click(function(e){
-    location.assign('./competition.php');
-  });
-  $('#newcomp').click(function(e){
-    location.assign('./new.php');
-  });
-  $('#dancer').click(function(e){
-    location.assign('./dancer.php');
-  });
-  $('#users').click(function(e){
-    location.assign('./lookup.php');
-  });
-  $('#univ').click(function(e){
-    location.assign('./new_univ.php');
-  });
-  </script>
-</body>
-</html>
+      <script>
+      $('#comp').click(function(e){
+        location.assign('./competition.php');
+      });
+      $('#newcomp').click(function(e){
+        location.assign('./new.php');
+      });
+      $('#dancer').click(function(e){
+        location.assign('./dancer.php');
+      });
+      $('#users').click(function(e){
+        location.assign('./lookup.php');
+      });
+      $('#univ').click(function(e){
+        location.assign('./new_univ.php');
+      });
+      </script>
+    </body>
+    </html>
